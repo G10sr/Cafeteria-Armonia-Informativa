b609d64a71c50059441254b2a51c7dfa600f6dfb
INNERGAMES <urandarien@gmail.com>
2026-04-17 09:40:45 -0600

    VAMOS FRAN

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ import logo from "../assets/img/arLogo.jpg"
 function Footer(){
     return(
         <div id="Footer">
-            <p>Armonía<br /> Todos los derechos reservados<br/>Num: 6110 3229</p>
+            <p>Armonía<br /> Todos los derechos reservados <br/>Num: 6110 3229</p>
             <img src={logo}></img>
         </div>
     )
